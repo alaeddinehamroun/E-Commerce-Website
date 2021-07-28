297cab8981eeb01b341a6d890554198f62fa07cf
alaeddine <alaeddinehamroun@insat.u-carthage.tn>
2021-07-28 23:43:50 +0100

    products + orders routes

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const logger = require('morgan');
+const app = express();
+const cors = require('cors');
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+
+/* CORS */
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+app.use(logger('combined'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Import Routes
+const productsRouter = require('./routes/products');
+const ordersRouter = require('./routes/orders');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/api/products', productsRouter);
+app.use('/api/orders', ordersRouter);
 
 module.exports = app;
---
+++ backend/routes/products.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var router = express.Router();
+
+const {database} = require('../config/helpers');
+
+/* GET ALL PRODUCTS. */
+router.get('/', function(req, res, next) {
+  //set the current page number
+  let page = (req.query.page !== undefined && req.query.page !==0) ?req.query.page :1;
+  //set the limiy of items per page
+  const limit = (req.query.limit !== undefined && req.query.limit !==0) ?req.query.limit :10;
+
+  let startValue;
+  let endValue;
+
+  if(page>0) {
+    startValue = (page * limit)-limit;
+    endValue = page * limit;
+  } else{
+    startValue = 0;
+    endValue = 10;
+  }
+  database.table('products as p ')
+    .join([{
+      table: 'categories as c',
+      on: 'c.id = p.cat_id'
+    }])
+    .withFields(['c.title as category',
+                  'p.title as name',
+                  'p.price',
+                  'p.quantity',
+                  'p.image',
+                  'p.id'
+                ])
+                .slice(startValue, endValue)
+                .sort({id : .1})
+                .getAll()
+                .then(prods => {
+                  if(prods.length > 0){
+                    res.status(200).json({
+                      count: prods.length,
+                      products: prods
+                    })
+                  } else {
+                    res.json({message: 'No products found'});
+                  }
+                }).catch(err => console.log(err));
+});
+/* GET SINGLE PRODUCT. */
+router.get('/:prodId', (req, res, next) => {
+  let productId = req.params.prodId;
+
+  database.table('products as p ')
+    .join([{
+      table: 'categories as c',
+      on: 'c.id = p.cat_id'
+    }])
+    .withFields(['c.title as category',
+                  'p.title as name',
+                  'p.price',
+                  'p.quantity',
+                  'p.image',
+                  'p.images',
+                  'p.id'
+                ])
+                .filter({'p.id': productId})
+                .get()
+                .then(prod => {
+                  if(prod){
+                    res.status(200).json(prod);
+                  } else {
+                    res.json({message: `No product found with product id ${productId}`});
+                  }
+                }).catch(err => console.log(err));
+
+});
+/* GET PRODUCTS BY CATEGORY */
+router.get('/category/:catName', (req, res, next) => {
+   //set the current page number
+   let page = (req.query.page !== undefined && req.query.page !==0) ?req.query.page :1;
+   //set the limiy of items per page
+   const limit = (req.query.limit !== undefined && req.query.limit !==0) ?req.query.limit :10;
+   let startValue;
+   let endValue;
+ 
+   if(page>0) {
+     startValue = (page * limit)-limit;
+     endValue = page * limit;
+   } else{
+     startValue = 0;
+     endValue = 10;
+   }
+   //fetch the category name from the url
+   const cat_title = req.params.catName;
+
+   database.table('products as p ')
+     .join([{
+       table: 'categories as c',
+       on: `c.id = p.cat_id WHERE c.title LIKE '%${cat_title}%' `
+     }])
+     .withFields(['c.title as category',
+                   'p.title as name',
+                   'p.price',
+                   'p.quantity',
+                   'p.image',
+                   'p.id'
+                 ])
+                 .slice(startValue, endValue)
+                 .sort({id : .1})
+                 .getAll()
+                 .then(prods => {
+                   if(prods.length > 0){
+                     res.status(200).json({
+                       count: prods.length,
+                       products: prods
+                     })
+                   } else {
+                     res.json({message: `No products found for category:${cat_title} `});
+                   }
+                 }).catch(err => console.log(err));
+});
+module.exports = router;
